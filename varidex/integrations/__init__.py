fad6ca98fa4887db7fca45f7c51d9ce8d22934fb
Plantucha <plantucha@gmail.com>
2026-01-22 16:45:00 -0500

    feat: Create integrations package for external APIs

---
+++ varidex/integrations/__init__.py
@@ -0,0 +1,8 @@
+"""External API integrations for VariDex.
+
+This package contains clients for external genomic databases and services.
+"""
+
+from varidex.version import __version__
+
+__all__ = ['__version__']
