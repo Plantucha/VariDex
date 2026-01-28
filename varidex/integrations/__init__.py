2deffedad400b861775938584eed5b70a325bceb
Plantucha <plantucha@gmail.com>
2026-01-28 16:21:09 -0500

    feat: Export gnomAD annotator in integrations module

--- varidex/integrations/__init__.py
+++ varidex/integrations/__init__.py
@@ -5,4 +5,30 @@ This package contains clients for external genomic databases and services.
 
 from varidex.version import __version__
 
-__all__ = ["__version__"]
+try:
+    from varidex.integrations.gnomad_annotator import (
+        GnomADAnnotator,
+        AnnotationConfig,
+        annotate_variants,
+    )
+    GNOMAD_ANNOTATOR_AVAILABLE = True
+except ImportError:
+    GNOMAD_ANNOTATOR_AVAILABLE = False
+
+try:
+    from varidex.integrations.gnomad_client import (
+        GnomadClient,
+        GnomadVariantFrequency,
+    )
+    GNOMAD_CLIENT_AVAILABLE = True
+except ImportError:
+    GNOMAD_CLIENT_AVAILABLE = False
+
+__all__ = [
+    "__version__",
+    "GnomADAnnotator",
+    "AnnotationConfig",
+    "annotate_variants",
+    "GnomadClient",
+    "GnomadVariantFrequency",
+]
