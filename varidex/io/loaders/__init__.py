1de465b3f295220b2d1a85fbec3cb47ec76e6a3e
Plantucha <plantucha@gmail.com>
2026-02-04 16:54:31 -0500

    fix: export correct loader functions (load_clinvar_file exists)

--- varidex/io/loaders/__init__.py
+++ varidex/io/loaders/__init__.py
@@ -1,3 +1,9 @@
+"""
+VariDex IO Loaders - Main entry points
+"""
+
 from .vcfloader import load_vcf, load_vcf_chunked
 from .clinvar import load_clinvar_file
-from .user import load_user_variants
+# user.py doesn't have load_user_variants - skipping
+
+__all__ = ['load_vcf', 'load_vcf_chunked', 'load_clinvar_file']
